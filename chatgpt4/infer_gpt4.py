923e6ba3fe9a0dfd4d607ae718f93451412c6095
dsonawan <dsonawan@nlp-gpu-01.be.ucsc.edu>
2023-10-31 13:22:47 -0700

    gpt4 tags

--- chatgpt4/infer_gpt4.py
+++ chatgpt4/infer_gpt4.py
@@ -37,7 +37,74 @@ def query_gpt4(row, logger):
     else:
         logger.error("Error - Conversaition ID - " + str(row[0]) + response.status_code)
     return chat_reply
-        
+
+def generate_tags(row, logger):
+    base_prompt_1 = """
+    Given a set of dialogues and its summary, the task is to do token-level classification based on whether it is hallucinated or not. Use the following tag classes to label each token of the summary. 
+    O = Not Hallucinated,
+    W =  Wrong person reference,
+    C = Circumstancial error,
+    OB = Object error,
+    N = uncommon errors like tense errors 
+    M = Missing information
+    The tag M should only be added at the end of the sequence incase the summary is missing any information and not as a tag specific to a word in the summary. 
+
+    Dialogue- "Hannah: Hey, do you have Betty's number?
+    Amanda: Lemme check
+    Hannah: <file_gif>
+    Amanda: Sorry, can't find it.
+    Amanda: Ask Larry
+    Amanda: He called her last time we were at the park together
+    Hannah: I don't know him well
+    Hannah: <file_gif>
+    Amanda: Don't be shy, he's very nice
+    Hannah: If you say so..
+    Hannah: I'd rather you texted him
+    Amanda: Just text him 馃檪
+    Hannah: Urgh.. Alright
+    Hannah: Bye
+    Amanda: Bye bye"
+
+    Summary- "Amanda can't find Betty's number. Larry called her last time they were at the park together. Amanda will text Larry."
+
+    Tags- "O O O O O O O O O O O O O O O O O O W O O O O"
+
+    Similarly, for the next dialogue and summary below, generate tags for the summary.
+
+    Dialogue-
+    """
+
+    base_prompt_2 = """
+    Summary- """
+
+    base_prompt_3 = """
+    Tags- """
+
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {
+            "role": "user",
+            "content": base_prompt_1 + str(row[1]) + base_prompt_2 + str(row[18]) + base_prompt_3
+            }
+        ],
+        temperature=0.001,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+        )
+    #result = response.json()
+    choices = response['choices']
+    chat_reply = ""
+    if len(choices) > 0:
+        chat_reply = choices[0]['message']['content']
+    else:
+        logger.error("Error - Conversaition ID - " + str(row[0]) + response.status_code)
+    return chat_reply
+
+
+
 def main():
     openai.api_key = "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"
     logger = logging.getLogger('my_logger')
@@ -52,7 +119,8 @@ def main():
     df = pd.read_csv(os.path.join(root_filepath, 'data', 'sample_annotated_capstone_data.csv')) 
 
     # Apply the custom function to each row along axis 1 (row-wise)
-    df['gpt_4_generated'] = df.apply(query_gpt4, axis=1, args=(logger,))
+    df['gpt_4_generated_summaries'] = df.apply(query_gpt4, axis=1, args=(logger,))
+    df['gpt_4_tags'] = df.apply(generate_tags, axis=1, args=(logger,))
     df.to_csv(os.path.join(root_filepath, 'data', 'gpt_4_summaries.csv'), index=False)
 
 main()
