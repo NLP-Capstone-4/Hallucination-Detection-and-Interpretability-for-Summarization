1416c131c2558b8b8890b787caab6256b8c9e008
dsonawan <dsonawan@nlp-gpu-01.be.ucsc.edu>
2023-11-14 14:10:27 -0800

    gpt4 tag generation

--- chatgpt4/infer_gpt4.py
+++ chatgpt4/infer_gpt4.py
@@ -360,6 +360,87 @@ def one_shot_summ_tags(row, logger):
         logger.error(e)
     return (summary, tags, explanation)
 
+def one_shot_tags(row, logger, model_name):
+    dialogue = row[1]
+    summary = row[3]
+    curr_model_name = row[-1].strip()
+
+    tags = None
+    explanation = None
+    if curr_model_name == model_name:
+        prompt_1 = """You are very good at conversation token classification. Given a conversation and its summary, the task is to do token-level classification based on whether it is hallucinated or not. Use the following tag classes to label each token of the summary.
+        
+        O = Not Hallucinated,
+        W =  Wrong person reference,
+        C = Circumstantial error,
+        OB = Object error,
+        N = uncommon errors like tense errors 
+        M = Missing information
+        The tag M should only be added at the end of the sequence incase the summary is missing any information and not as a tag specific to a word in the summary. 
+
+        Conversation- "Hannah: Hey, do you have Betty's number?
+        Amanda: Lemme check
+        Hannah: <file_gif>
+        Amanda: Sorry, can't find it.
+        Amanda: Ask Larry
+        Amanda: He called her last time we were at the park together
+        Hannah: I don't know him well
+        Hannah: <file_gif>
+        Amanda: Don't be shy, he's very nice
+        Hannah: If you say so..
+        Hannah: I'd rather you texted him
+        Amanda: Just text him 馃檪
+        Hannah: Urgh.. Alright
+        Hannah: Bye
+        Amanda: Bye bye"
+
+        Summary- "Amanda can't find Betty's number. Larry called her last time they were at the park together. Amanda will text Larry."
+
+        Tags- "O O O O O O O O O O O O O O O O O O W O O O O"
+
+        Explanation- Let's think step by step. The dialogue is about Hannah asking for Betty's number to Amanda, who couldn't find it and suggests to ask Larry for it since he had called her(Betty) the last time they were in the park together. Hannah doesn't know him(Larry) well and is shy to text him, but Amanda asks her to do it anyway.  So according to the summary, "Amanda will text Larry" is incorrect. The way to correct this information is the token Amanda can be changed to Hannah. This is Wrong Reference (W) from the tokens described above. All other tokens are correct and are thus Not Hallucinated (O).
+
+        Similarly, for the next dialogue, generate tags for the summary. Think step by step to explain it.
+        Conversation- 
+        """
+        
+        prompt_2 = """
+        Summary- 
+        """
+
+        prompt_3 = """
+        Tags- 
+
+        Explanation- 
+        """
+
+        try:
+            response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=[
+                {
+                "role": "user",
+                "content": prompt_1 + str(row[1]) + prompt_2 + str(row[3]) + prompt_3
+                }
+            ],
+            temperature=0.001,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+            )
+            #result = response.json()
+            choices = response['choices']
+            if len(choices) > 0:
+                data = choices[0]['message']['content']
+                (summary, tags, explanation) = extract_summary_tags(data) # summary will be always None bcz its not generated
+                logger.info("Successfully infered conv_id:" + str(row[0]))
+            else:
+                logger.error("Error - Conversaition ID - " + str(row[0]) + response.status_code)
+        except Exception as e:
+            logger.error(e)
+    return (tags, explanation)
+
 def main():
     openai.api_key = "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"
     code_filepath = os.path.dirname(os.path.abspath(__file__))
@@ -375,23 +456,25 @@ def main():
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler) 
     
+    model_name = "BART-Cons"
     data_filepath = os.path.join(root_filepath, 'data', 'annotated_capstone_data.csv')
-    save_filepath = os.path.join(root_filepath, 'data', 'one_shot_summ_tags.csv')
+    save_filepath = os.path.join(root_filepath, 'data', 'one_shot_tags_for_' + model_name + '_summary.csv')
     if not os.path.exists(save_filepath):
-        save_df = pd.DataFrame(columns=['ID', 'gpt_4_generated_summaries', 'gpt_4_tags', 'gpt_4_explanation'])
+        save_df = pd.DataFrame(columns=['ID', 'gpt_4_tags', 'gpt_4_explanation'])
         save_df.to_csv(save_filepath, index=False)
     already_processed_conv_id = set(pd.read_csv(save_filepath)['ID'])
 
     df = pd.read_csv(data_filepath) 
     df = df.head(n=100)
     
+    
     with open(save_filepath, 'a') as fp:
         writer = csv.writer(fp)
         for index, row in df.iterrows():
             conv_id = row[0]
             if conv_id not in already_processed_conv_id:
-                (summary, tags, explanation) = one_shot_summ_tags(row, logger)
-                writer.writerow([conv_id, summary, tags, explanation])
+                (tags, explanation) = one_shot_tags(row, logger, model_name)
+                writer.writerow([conv_id, tags, explanation])
                 time.sleep(10)
 
     # Apply the custom function to each row along axis 1 (row-wise)
