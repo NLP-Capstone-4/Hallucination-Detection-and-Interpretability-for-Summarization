defe70db93f930400f338a54cea857f05cf48a9c
bgarg <bgarg@nlp-gpu-02.be.ucsc.edu>
2023-12-03 20:36:19 -0800

    prompt experimentation

--- llama_inference.py
+++ llama_inference.py
@@ -36,58 +36,92 @@ pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_le
 
 
 
-prompt_template ="""
-### Instruction:
-Given a set of dialogues, the task is to generate a summary of 10-15 words by considering all the dialogues, and do token-level classification on the summary based on whether it is hallucinated or not. Use the following tag classes to label each token of the summary.
-O = Not Hallucinated,
-W =  Wrong person reference,
-C = Circumstantial error,
-OB = Object error,
-N = uncommon errors like tense errors
-M = Missing information
-The tag M should only be added at the end of the sequence incase the summary is missing any information and not as a tag specific to a word in the summary.
+# prompt_template ="""
+# ### Instruction:
+# Given a dialogue, the task is to generate a summary of 10-15 words. Further, label each token of this generated summary based on the following given tags.
+# W =  Wrong person reference,
+# C = Circumstantial error,
+# OB = Object error,
+# N = uncommon errors like tense errors
+# M = Missing information
+# The tag M should only be added at the end of the sequence incase the summary is missing any information and not as a tag specific to a word in the summary.
 
-### Input:
-[INST]
-Dialogue- "Hannah: Hey, do you have Betty's number?
-Amanda: Lemme check
-Hannah: <file_gif>
-Amanda: Sorry, can't find it.
-Amanda: Ask Larry
-Amanda: He called her last time we were at the park together
-Hannah: I don't know him well
-Hannah: <file_gif>
-Amanda: Don't be shy, he's very nice
-Hannah: If you say so..
-Hannah: I'd rather you texted him
-Amanda: Just text him 馃檪
-Hannah: Urgh.. Alright
-Hannah: Bye
-Amanda: Bye bye"
-[/INST]
+# ### Input:
+# [INST]
+# Dialogue- "Hannah: Hey, do you have Betty's number?
+# Amanda: Lemme check
+# Hannah: <file_gif>
+# Amanda: Sorry, can't find it.
+# Amanda: Ask Larry
+# Amanda: He called her last time we were at the park together
+# Hannah: I don't know him well
+# Hannah: <file_gif>
+# Amanda: Don't be shy, he's very nice
+# Hannah: If you say so..
+# Hannah: I'd rather you texted him
+# Amanda: Just text him 馃檪
+# Hannah: Urgh.. Alright
+# Hannah: Bye
+# Amanda: Bye bye"
+# [/INST]
 
-###Response: 
-Summary- "Amanda can't find Betty's number. Larry called her last time they were at the park together. Amanda will text Larry."
+# ###Response: 
+# Summary- "Amanda can't find Betty's number. Larry called her last time they were at the park together. Amanda will text Larry."
 
-Tags- "O O O O O O O O O O O O O O O O O O W O O O O"
+# Tags- "O O O O O O O O O O O O O O O O O O W O O O O"
 
-Explanation - Let's think step by step. The dialogue is about Hannah asking for Betty's number to Amanda, who couldn't find it and suggests to ask Larry for it since he had called her(Betty) the last time they were in the park together. Hannah doesn't know him(Larry) well and is shy to text him, but Amanda asks her to do it anyway.  So according to the summary, "Amanda will text Larry" is incorrect. The way to correct this information is the token Amanda can be changed to Hannah. This is Wrong Reference (W) from the tokens described above. All other tokens are correct and are thus Not Hallucinated (O).
 
-[INST]
-Dialogue- "Harry: and? have you listened to it?
-Jacob: listened to what?
-Harry: to the song i sent you 3 days ago -.-
-Jacob: oh shit, i completely forgot...
-Harry: ofc again
-Jacob: don't be like this :* i'll do that later tonight
-Harry: heh, okay
-Harry: i'm really curious what you'll think about it
-Jacob: i'll let you know, a bit busy right now, speak to you later!
-Harry: okay"
-[/INST]
+# [INST]
+# Dialogue- "Harry: and? have you listened to it?
+# Jacob: listened to what?
+# Harry: to the song i sent you 3 days ago -.-
+# Jacob: oh shit, i completely forgot...
+# Harry: ofc again
+# Jacob: don't be like this :* i'll do that later tonight
+# Harry: heh, okay
+# Harry: i'm really curious what you'll think about it
+# Jacob: i'll let you know, a bit busy right now, speak to you later!
+# Harry: okay"
+# [/INST]
 
-###Response: 
-"""
+# ###Response: 
+
+# """
+
+
+
+
+# prompt_template ="""
+# ### Task Description:
+# Given a dialogue, generate a concise summary consisting of 10-15 words. Additionally, assign annotation to each word obtained by spliting on space in the generated summary using the following annotations:
+# O = Not Hallucinated,
+# W = Wrong person reference,
+# C = Circumstantial error,
+# OB = Object error,
+# N = Uncommon errors like tense errors,
+# M = Missing information
+
+
+# If the summary is "how are you" then should be "O O O".Number of annotations should be equal to the number of words in the summary
+# Note: Tag "M" should only be appended at the end of the sequence if the summary lacks any essential information, not as a tag assigned to any specific word in the summary.
+
+
+# [INST]
+# Dialogue- "Harry: and? have you listened to it?
+# Jacob: listened to what?
+# Harry: to the song i sent you 3 days ago -.-
+# Jacob: oh shit, i completely forgot...
+# Harry: ofc again
+# Jacob: don't be like this :* i'll do that later tonight
+# Harry: heh, okay
+# Harry: i'm really curious what you'll think about it
+# Jacob: i'll let you know, a bit busy right now, speak to you later!
+# Harry: okay"
+# [/INST]
+
+# ###Response: 
+
+# """
 
 input_sentence = prompt_template
 
