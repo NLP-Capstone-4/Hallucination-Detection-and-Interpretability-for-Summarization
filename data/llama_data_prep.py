d39e5fe378846c9a4964e6d574bf3fec6ab94b66
bgarg <bgarg@nlp-gpu-02.be.ucsc.edu>
2023-12-03 20:07:55 -0800

    Create llama training data

---
+++ data/llama_data_prep.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+file_path = '/home/bgarg/custom_new/Hallucination-Detection-and-Interpretability-for-Summarization/data/annotated_capstone_data.csv' 
+data = pd.read_csv(file_path)
+data = data[['ID','Dialogue','Generated Summary', 'Annotations']]
+
+data['Combined'] = "<START_C>" + data['Dialogue'] + "<END_Q>  <START_S>" + data['Generated Summary'] + "<END_S>  <START_A>" + data['Annotations'] + "<END_A>"
+
+
+
+train = data.iloc[:400,:]
+val = data.iloc[400:500,:]
+test = data.iloc[500:,:]
+
+train.to_csv('/home/bgarg/custom_new/Hallucination-Detection-and-Interpretability-for-Summarization/data/train_llama_data.csv', index=False, encoding="utf8")
+val.to_csv('/home/bgarg/custom_new/Hallucination-Detection-and-Interpretability-for-Summarization/data/val_llama_data.csv', index=False, encoding="utf8")
+test.to_csv('/home/bgarg/custom_new/Hallucination-Detection-and-Interpretability-for-Summarization/data/test_llama_data.csv', index=False, encoding="utf8")
